60938fa126da42a1d351f393152589c2fd6e0be7
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-03-21 19:29:35 +0300

    Completed Assigment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  function onFocused(event) {
+    // add event handlers that listen for the focus and blur events.
+    console.log("Good!");
+  }
+  function onBlurred(event) {
+    console.log("Hey! Eyes on me!");
+  }
+  return (
+    <>
+      <button onBlur={onBlurred} onFocus={onFocused}>
+        Eyes on me
+      </button>
+    </>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handleChange(event) {
+    console.log("Entering password...");
+  }
+
+  return (
+    <div>
+      <input onChange={handleChange} type='password' />
+    </div>
+  );
 }
 
 export default Keypad;
